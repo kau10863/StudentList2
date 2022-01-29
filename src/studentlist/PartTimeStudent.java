3ce2965c1dd30541aa61d763d3db24220dc0a25e
navjeet <kau10863@sheridancollege.ca>
2022-01-29 14:54:01 -0500

    added a comment

--- src/studentlist/PartTimeStudent.java
+++ src/studentlist/PartTimeStudent.java
@@ -6,6 +6,8 @@ package studentlist;
  * @author navjeetkaur jan 2022
  */
 public class PartTimeStudent extends Student{
+    
+    //This is done
     private int numCourses;
     public PartTimeStudent(int numCourses, String name) {
         super(name);
